30349213f9b65d58ef34d397cebb170fb6801834
Ansh Srivastava <anshs1939@gmail.com>
2023-04-09 01:25:54 +0530

    creating & importing components & making chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Box , Button, Container , VStack , Input, HStack} from "@chakra-ui/react";
+import Message from "./Component/Message";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={"red.50"} >
+      <Container h={"100vh"} bg={"white"}>
+      <VStack h="full" paddingY={"4"}>
+        <Button w="full" colorScheme={"red"}> Logout </Button>
+        <VStack h="full" w={"full"} overflowY={"auto"}>
+          <Message text={"Simple messade"}/>
+          <Message user="me" text={"Simple messade"}/>
+          <Message text={"Simple messade"}/>
+        </VStack>
+          
+          
+          <form style={{width:"100%"}}>
+            <HStack>
+              <Input placeholder="Enter a message..."/>
+                <Button colorScheme={"purple"} type="submit">Send
+                </Button>
+            </HStack>
+           
+          </form>
+          
+        
+      </VStack>
+      </Container>
+    </Box>
   );
 }
 
